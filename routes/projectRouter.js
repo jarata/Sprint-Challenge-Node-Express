29419a5088ab86d4e3389001983e071f98f4e313
J A <jarata@gmail.com>
2019-02-22 10:43:24 -0800

    projects/:id DELETE

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -124,8 +124,24 @@ router.put('/:id', async (req, res) => {
 //       .del();
 //   },
 
-// router.remove('/:id', async (req, res) => {
-//
-// });
+router.delete('/:id', async (req, res) => {
+    try {
+        const count = await Projects.remove(req.params.id);
+        if (count > 0) {
+            res.status(200).json({
+                message: "Project successfully deleted"
+            })
+        } else {
+            res.status(404).json({
+                error: "The project with the specified ID does not exist"
+            })
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            error: "The project could not be deleted"
+        })
+    }
+});
 
 module.exports = router;
